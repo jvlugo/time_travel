d1ae23cbb0b50488f130986f044c3ab39f711035
Jesus Villamor <Jesus.Villamor@ust-global.com>
2017-06-14 10:24:49 +0200

    time_travel_spec.rb commit

--- spec/time_travel_spec.rb
+++ spec/time_travel_spec.rb
@@ -2,6 +2,7 @@ describe TimeTravel do
 
 it "should travel from one year to another"
 it "should keep age of traveller when moving between years"
-it "should return traveler to current year on exit"
+it "should return traveller to current year on exit"
+it "should keep object type of traveller"
 
 end
