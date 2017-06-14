d39b2dd6a347dc44594bf788d1387acb05cb0ed7
Jesus Villamor <Jesus.Villamor@ust-global.com>
2017-06-14 08:50:37 +0200

    traveller to currentyear

--- spec/time_travel_spec.rb
+++ spec/time_travel_spec.rb
@@ -2,5 +2,6 @@ describe TimeTravel do
 
 it "should travel from one year to another"
 it "should keep age of traveller when moving between years"
+it "should return traveler to current year on exit"
 
 end
